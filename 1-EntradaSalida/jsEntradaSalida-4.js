d5ee3e25144e0eecae6059524e1d8042cbbb8166
Jhossymarbalderrama <52534649+Jhossymarbalderrama@users.noreply.github.com>
2019-07-04 12:16:23 -0300

    Ejercicios 3, 4 y 5

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -1,9 +1,13 @@
-/* 
-	Debemos lograr tomar un dato por 'PROMPT' 
+/*
+	Debemos lograr tomar un dato por 'PROMPT'
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
+  var dato;
 
+  dato = prompt("ingrese su nombre","Ingrese aquí su nombre");
+
+  document.getElementById("elNombre").value = dato ;
 }
 
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,14 @@
-/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
-{	
-	
+{
+  var elNombre;
+  var laEdad;
+
+  elNombre = document.getElementById("elNombre").value ;
+  laEdad = document.getElementById("laEdad").value ;
+
+  alert("Usted se llama "+elNombre+" y tiene "+laEdad);
+
 }
 
